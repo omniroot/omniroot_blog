988bac1ff3466dc68727b490fdf4d427362d098c
omniroot <omnirootofc@gmail.com>
2025-03-02 21:48:58 +0300

    Update about page

--- src/app/pages/about/about.page.tsx
+++ src/app/pages/about/about.page.tsx
@@ -1,23 +1,23 @@
+import { useLanguage } from "@/shared/stores/language.store.tsx";
 import { createLazyRoute } from "@tanstack/react-router";
 import styles from "./about.page.module.css";
-import { useTranslation } from "react-i18next";
 
 export const AboutPage = () => {
-  const { t } = useTranslation();
+  const { t } = useLanguage();
   return (
     <div className={styles.content}>
-      <span>{t("iam")}</span>
-      <span>&nbsp; {t("iam_content")}</span>
-      <span>{t("experience")}</span>
-      <span>&nbsp; {t("experience_content_1")}</span>
-      <span>&nbsp; {t("experience_content_2")}</span>
-      <span>&nbsp; {t("experience_content_3")}</span>
-      <span>{t("background")}</span>
-      <span>&nbsp; {t("background_content")}</span>
-      <span>{t("interests")}</span>
-      <span>&nbsp; {t("interests_content")}</span>
-      <span>{t("careerGoal")}</span>
-      <span>&nbsp; {t("careerGoal_content")}</span>
+      <span className={styles.title}>{t("iam")}</span>
+      <span className={styles.body}>{t("iam_content")}</span>
+      <span className={styles.title}>{t("experience")}</span>
+      <span className={styles.body}>{t("experience_content_1")}</span>
+      <span className={styles.body}>{t("experience_content_2")}</span>
+      <span className={styles.body}>{t("experience_content_3")}</span>
+      <span className={styles.title}>{t("background")}</span>
+      <span className={styles.body}>{t("background_content")}</span>
+      <span className={styles.title}>{t("interests")}</span>
+      <span className={styles.body}>{t("interests_content")}</span>
+      <span className={styles.title}>{t("careerGoal")}</span>
+      <span className={styles.body}>{t("careerGoal_content")}</span>
     </div>
   );
 };
